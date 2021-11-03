116a377d24dd330651948daee0765e84cf481a56
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-03 09:18:56 -0400

    Update footer.blade.php

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -8,13 +8,13 @@
             <!-- Required Unicons (if you want) -->
             <div class="flex items-center space-x-3 text-sm">
                 <div class="hidden md:block">Follow us: </div>
-                <a href="https://twitter.com/traversymedia" class="flex items-center mx-1 space-x-2">
+                <a href="https://twitter.com/AlecCam43544378" class="flex items-center mx-1 space-x-2">
                     <img src="/assets/images/twitter.svg" alt="twitter">
-                    <div>traversymedia</div>
+                    <div>AlecCam43544378</div>
                 </a>
-                <a href="https://twitter.com/csaba_kissi" class="flex items-center mx-1 space-x-2">
+                <a href="https://twitter.com/AlecCam43544378" class="flex items-center mx-1 space-x-2">
                     <img src="/assets/images/twitter.svg" alt="twitter">
-                    <div>Csaba</div>
+                    <div>AlecCam43544378</div>
                 </a>
                 <a href="https://www.youtube.com/user/TechGuyWeb" class="flex items-center mx-1 space-x-2 text-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
